28c5358e5d437e8d9442a0c34bbd2a2e8708c628
Kartavya Shukla <87070473+Novfensec@users.noreply.github.com>
2024-07-10 19:04:10 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,47 @@
+from kivymd.app import MDApp
+from kaki.app import App
+from kivy.factory import Factory
+import os
+from kivymd.uix.screen import MDScreen
+
+class HomeScreen(MDScreen):
+    def __init__(self, *args,**kwargs):
+        super(HomeScreen, self).__init__(*args,**kwargs)
+
+class LoginScreen(MDScreen):
+    def __init__(self, *args,**kwargs):
+        super(LoginScreen, self).__init__(*args,**kwargs)
+
+class UI(Factory.ScreenManager):
+    def __init__(self, *args,**kwargs):
+        super(UI, self).__init__(*args,**kwargs)
+
+class Novfensec(MDApp,App):
+    def __init__(self, *args,**kwargs):
+        super(Novfensec, self).__init__(*args,**kwargs)
+        self.KV_FILES={
+            os.path.join(os.getcwd(),"kv_files/app.kv")
+            #all kv files path here
+        }
+        self.CLASSES={
+            "UI":"main" # main file name or root file name
+        }
+        self.AUTORELOADER_PATHS=[
+            (".",{"recursive":True}),
+        ]
+        self.DEBUG=True
+        
+    def build_app(self):
+        self.manager_screens=Factory.UI()
+        self.theme_cls.primary_palette="Indigo"
+        self.generate_application_screens()
+        self.manager_screens.current="home screen"
+        return self.manager_screens
+    
+    def generate_application_screens(self):
+        self.manager_screens.add_widget(HomeScreen(name="home screen"))
+        self.manager_screens.add_widget(LoginScreen(name="login screen"))
+
+if __name__ == '__main__':
+    Novfensec().run()
+    
